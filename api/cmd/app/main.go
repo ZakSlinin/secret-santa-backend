42aab8f6aefc9fde51ab5208ec370cea28c37fc8
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-11-16 00:10:59 +0300

    feat: migrations

---
+++ api/cmd/app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/api/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
+	})
+	http.ListenAndServe(":8080", r)
+}
---
+++ api/migrations/00001_add_columns.sql
@@ -0,0 +1,32 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+                       id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                       user_id TEXT NOT NULL,
+                       name TEXT NOT NULL,
+                       username TEXT NOT NULL,
+                       groups_member TEXT[] DEFAULT '{}'
+);
+
+CREATE TABLE groups (
+                        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                        name TEXT NOT NULL,
+                        group_id UUID DEFAULT uuid_generate_v4(),
+                        members TEXT[] DEFAULT '{}',
+                        admin TEXT
+);
+
+CREATE TABLE group_members (
+                               group_id UUID REFERENCES groups(id) ON DELETE CASCADE,
+                               user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+                               PRIMARY KEY (group_id, user_id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS groups;
+-- +goose StatementEnd
